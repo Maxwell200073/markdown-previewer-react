f4bf2590a773c272ac3e77ef7f46b8d151a56403
Maxwell200073 <91772625+Maxwell200073@users.noreply.github.com>
2022-01-07 22:25:05 -0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
+import ReactMarkdown from 'react-markdown'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  
+  const mark = `# React Markdown Previewer
+## This is an h2 element
+### h3 You get the idea
+Heres some code, \`<div></div>\`, between 2 backticks.
+
+This is **bold**
+This is _italic_
+\`\`\`
+code inside
+\`\`\`
+[This is a link](https://freecodecamp.org)
+> Block Quotes!
+
+![Coding Image](https://www.elegantthemes.com/blog/wp-content/uploads/2020/02/000-Online-Code-Editors.png "Picture of a laptop and coffee")
+- One
+- Two 
+
+`
+  const [markdown, setMarkdown] = useState(mark)
+  return <main>
+    <section className='markdown'>
+      <textarea className='input' value={markdown} id='editor' onChange={(e)=>setMarkdown(e.target.value)}></textarea>
+    <article className='result' id='preview'>
+
+    <ReactMarkdown >{markdown}</ReactMarkdown>
+    </article>
+    </section>
+  </main>
 }
 
-export default App;
+export default App
